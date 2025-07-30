cf94b33d5611d65d60ba9d3ce6ff8e44fe7c82ae
Irenka Ni <ni.irenka@gmail.com>
2025-07-30 15:06:06 -0700

    last push no backened

---
+++ server/src/controllers/location.controller.ts
@@ -0,0 +1,62 @@
+import express from 'express';
+import { ILocation } from '../models/location.model.ts';
+import ApiError from '../util/apiError.ts';
+import {
+    createLocation,
+    getAllLocations,
+    getLocationByName,
+    deleteLocationByName,
+} from '../services/location.service';
+import StatusCode from '../util/statusCode.ts';
+
+const addLocation = async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+) => {
+    const Location: ILocation | null = req.body as ILocation;
+    if (!Location) {
+        next(ApiError.missingFields(['Location']));
+        return;
+    }
+    // eslint-disable-next-line consistent-return
+    return createLocation(Location);
+};
+
+const getLocations = async (
+    req: express.Request,
+    res: express.Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    next: express.NextFunction,
+) => {
+    const people = await getAllLocations(); // Call the service function to get data
+    res.status(200).json({ data: people });
+};
+
+const deleteLocation = async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+) => {
+    const { Name } = req.params;
+    if (!Name) {
+        next(ApiError.missingFields(['Name']));
+        return;
+    }
+
+    // Check if location to delete is an a valid location
+    const location: ILocation | null = await getLocationByName(Name);
+    if (!location) {
+        next(ApiError.notFound(`User with location ${location} does not exist`));
+        return;
+    }
+
+    deleteLocationByName(location.Name)
+        .then(() => res.sendStatus(StatusCode.OK))
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        .catch((e: unknown) => {
+            next(ApiError.internal('Failed to delete location.'));
+        });
+};
+
+export { addLocation, getLocations, deleteLocation };
